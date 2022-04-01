4b945e57103a0a9636fb95342ed85c0b626d1c82
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-04-02 01:10:49 +0530

    Security Change

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(cors())
 app.use('/books', router)        //http://localhost:3000/books
 
-mongoose.connect("mongodb+srv://admin:123@cluster0.oka1p.mongodb.net/bookStore?retryWrites=true&w=majority")
+mongoose.connect(MongoDBURL)
     .then(() => console.log("Connected To Database"))
     .then(()=>{
         app.listen(3000);
